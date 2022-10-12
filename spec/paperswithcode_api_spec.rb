9b9c6d6a1d73c69e20ab8d99cc56e4961183ef7f
Demi871023 <demi871023@gmail.com>
2022-10-13 02:37:11 +0800

    implement test

---
+++ spec/paperswithcode_api_spec.rb
@@ -0,0 +1,57 @@
+# frozen_string_literal: true
+
+require 'minitest/autorun'
+require 'minitest/rg'
+require 'yaml'
+require_relative '../lib/paperswithcode_api'
+PAPERNAME = 'be-your-own-teacher-improve-the-performance'
+CORRECT = YAML.safe_load(File.read('fixtures/results.yml'))
+# CONFIG = YAML.safe_load(File.read('config/secrets.yml'))
+describe 'Test Paperswithcode API Library' do
+  describe 'Paper information' do
+    it 'HAPPY: should provide correct paper information' do
+      paper = PapMon::PaperswithcodeApi.new.paper(PAPERNAME)
+      _(paper.id).must_equal CORRECT['id']
+      _(paper.arxiv_id).must_equal CORRECT['arxiv_id']
+      _(paper.url_abs).must_equal CORRECT['url_abs']
+      _(paper.title).must_equal CORRECT['title']
+      _(paper.authors).must_equal CORRECT['authors']
+      _(paper.published).must_equal CORRECT['published']
+      _(paper.proceeding).must_equal CORRECT['proceeding']
+    end
+
+    it 'SAD: should raise exception on invalid paper name' do
+      _(proc {
+          PapMon::PaperswithcodeApi.new.paper('be-your-own-teacher')
+        }).must_raise PapMon::PaperswithcodeApi::Errors::NotFound
+    end
+  end
+
+  describe 'Dataset information' do
+    before do
+      @paper = PapMon::PaperswithcodeApi.new.paper(PAPERNAME)
+    end
+
+    it 'HAPPY: should provide correct dataset information' do
+      datasets = @paper.datasets
+      _(datasets.count).must_equal CORRECT['datasets'].count
+      datasets_names = datasets.map(&:name)
+      correct_datasets_names = CORRECT['datasets'].map { |d| d['name'] }
+      _(datasets_names).must_equal correct_datasets_names
+    end
+  end
+
+  describe 'Repository information' do
+    before do
+      @paper = PapMon::PaperswithcodeApi.new.paper(PAPERNAME)
+    end
+
+    it 'HAPPY: should provide correct repository information' do
+      repos = @paper.repositories
+      _(repos.count).must_equal CORRECT['repositories'].count
+      repos_names = repos.map(&:repo_name)
+      correct_repos_names = CORRECT['repositories'].map { |d| d['name'] }
+      _(repos_names).must_equal correct_repos_names
+    end
+  end
+end
