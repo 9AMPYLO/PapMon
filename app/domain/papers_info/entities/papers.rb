e915a8e8e48620e7ee5d8707e4d58a9c481ad21d
Jerry <jerry940100@gmail.com>
2022-11-14 15:15:04 +0800

    run rubocup

--- app/domain/papers_info/entities/papers.rb
+++ app/domain/papers_info/entities/papers.rb
@@ -4,48 +4,50 @@
 module PapMon
   # Entity module
   module Entity
+    # Papers Entity
     class Papers < SimpleDelegator
       attr_reader :papers
 
       def initialize(papers:)
         # array of paper entities
         # [paper1, paper2, paper3, ....]
+        super(papers)
         @papers = papers
       end
 
       def category_and_count
-        papersCV = []
-        papersAI = []
-        papersLG = []
-        papersCL = []
-        papersNE = []
-        papersML = []
-        papersOTHER = []
+        papers_cv = []
+        papers_ai = []
+        papers_lg = []
+        papers_cl = []
+        papers_ne = []
+        papers_ml = []
+        papers_other = []
         @papers.each do |paper|
           case paper.primary_category
           when Value::Category::CV.name
-            papersCV << paper
+            papers_cv << paper
           when Value::Category::AI.name
-            papersAI << paper
+            papers_ai << paper
           when Value::Category::LG.name
-            papersLG << paper
+            papers_lg << paper
           when Value::Category::CL.name
-            papersCL << paper
+            papers_cl << paper
           when Value::Category::NE.name
-            papersNE << paper
+            papers_ne << paper
           when Value::Category::ML.name
-            papersML << paper
+            papers_ml << paper
           else
-            papersOTHER << paper
+            papers_other << paper
           end
         end
-        { 'paperCV' => papersCV, 'nCV' => papersCV.length,
-          'paperAI' => papersAI, 'nAI' => papersAI.length,
-          'paperLG' => papersLG, 'nLG' => papersLG.length,
-          'paperCL' => papersCL, 'nCL' => papersCL.length,
-          'paperNE' => papersNE, 'nNE' => papersNE.length,
-          'paperML' => papersML, 'nML' => papersML.length,
-          'paperOTHER' => papersOTHER, 'nOTHER' => papersOTHER.length }
+        { 'paperCV' => papers_cv, 'nCV' => papers_cv.length,
+          'paperAI' => papers_ai, 'nAI' => papers_ai.length,
+          'paperLG' => papers_lg, 'nLG' => papers_lg.length,
+          'paperCL' => papers_cl, 'nCL' => papers_cl.length,
+          'paperNE' => papers_ne, 'nNE' => papers_ne.length,
+          'paperML' => papers_ml, 'nML' => papers_ml.length,
+          'paperOTHER' => papers_other, 'nOTHER' => papers_other.length }
       end
     end
   end
